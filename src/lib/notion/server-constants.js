a148db87242098e8bddb03b76f947dcff2098231
Takumi Hamamura <thamamur@TakuminoMacBook-Pro-2.local>
2020-09-07 20:50:25 +0900

    update env

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -15,8 +15,10 @@ const normalizeId = id => {
   )}-${id.substr(20)}`
 }
 
-const NOTION_TOKEN = process.env.NOTION_TOKEN
-const BLOG_INDEX_ID = normalizeId(process.env.BLOG_INDEX_ID)
+// const NOTION_TOKEN = process.env.NOTION_TOKEN
+// const BLOG_INDEX_ID = normalizeId(process.env.BLOG_INDEX_ID)
+const NOTION_TOKEN = '0efe62d74380191855a8b8caccb38e36fafc940e72ea2cf03c71cf0a2b7210803911b60a9887f7402374f4ae49d8f6dfd53ea6606871e711716e23c822a5cf2a99877ef1fc31044eb573a8eb2763'
+const BLOG_INDEX_ID = '9f2df00f3fc948f28d6af3c0756c7570'
 const API_ENDPOINT = 'https://www.notion.so/api/v3'
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 
